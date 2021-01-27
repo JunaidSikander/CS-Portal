185ad023d6bb3b10d5b68bac2f4764af2774eefa
Junaid Sikander <Junaid_sikander@live.com>
2021-01-27 22:40:19 +0500

    [ADD] routing & redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap.min.css';
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
