e4a207017e9a7484a0790fb5ada94cf5a28a6c68
Junaid Sikander <Junaid_sikander@live.com>
2021-01-27 22:39:14 +0500

    [UPDATE] configuration

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import ConnectDB from "./config/db.js";
+import userRouter from './routes/userRoutes.js'
 import {config} from 'dotenv';
+import {errorHandler, notFound} from "./middlewares/errorMiddleware.js";
 
 
 config();
+ConnectDB();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'development') {
@@ -14,6 +19,9 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use('/api/user', userRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is up in ${process.env.NODE_ENV} mode on Port ${port}`);
