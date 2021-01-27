c3d4125a75ff217e930be5c33896b123512817a3
Junaid Sikander <Junaid_sikander@live.com>
2021-01-27 22:40:52 +0500

    [UPDATE] with routuing screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-function App() {
+import React from 'react';
+import {Container} from 'react-bootstrap'
+import Header from "./components/Header";
+import {Route} from "react-router-dom";
+import Home from "./views/Home";
+import Footer from "./components/Footer";
+import Signin from "./views/Signin";
+import Signup from "./views/Signup";
+
+const App =() => {
     return (
         <>
-            <h1>Hello World </h1>
+            <Header/>
+            <main className='py-3'>
+                <Container>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/login" component={Signin}/>
+                    <Route path="/register" component={Signup}/>
+                </Container>
+            </main>
+            <Footer/>
         </>
+
     );
 }
 
